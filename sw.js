efe88aa8f856fc21e2992f5e5ef5c84285383fee
setter-theory <5chvws9td6@privaterelay.appleid.com>
2026-08-03 14:20:21 +0900

    V54.3

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE = 'teamtheory-v0.54.2';
+const CACHE = 'teamtheory-v0.54.3';
 const APP_SHELL = [
   './',
   './index.html',
-  './styles.css?v=0.54.2',
-  './app.js?v=0.54.2',
-  './supabase-config.js?v=0.54.2',
-  './manifest.webmanifest?v=0.54.2',
+  './styles.css?v=0.54.3',
+  './app.js?v=0.54.3',
+  './supabase-config.js?v=0.54.3',
+  './manifest.webmanifest?v=0.54.3',
   './icons/icon-192.png',
   './icons/icon-512.png',
   './icons/apple-touch-icon.png',
